57a8cbc90f604cfa7275c7f2b896ea5eddeb354d
Annelies <118388665+Starianne@users.noreply.github.com>
2025-10-07 09:39:48 +0100

    Added Django

---
+++ birthdaycarddjango/birthdaycarddjango/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for birthdaycarddjango project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'birthdaycarddjango.settings')
+
+application = get_asgi_application()
