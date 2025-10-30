60d74c20caa9fcf093de397d6721e176a9dd16be
Annelies <118388665+Starianne@users.noreply.github.com>
2025-10-30 08:56:30 +0000

    trying to fix url bug

--- bdaycardproject/bdaycardproject/urls.py
+++ bdaycardproject/bdaycardproject/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for birthdaycarddjango project.
+URL configuration for bdaycardproject project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
@@ -15,10 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("members/", include("birthdaycarddjango.members.urls")),
-
+    path('', views.homepage),
+    path('about/', views.about),
 ]
--- bdaycardproject/manage.py
+++ bdaycardproject/manage.py
@@ -3,11 +3,10 @@
 import os
 import sys
 
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'birthdaycarddjango.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bdaycardproject.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
