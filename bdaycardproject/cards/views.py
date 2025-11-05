69ec4cfa00cffe24d0e45871baa55dd4fe091ccf
Annelies <118388665+Starianne@users.noreply.github.com>
2025-11-05 12:45:57 +0000

    new issue with cards not showing, but got them working as models!

--- bdaycardproject/cards/views.py
+++ bdaycardproject/cards/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Cards
 
 # Create your views here.
 def cards_list(request):
-    return render(request, 'cards/cards_list.html')
+    cards = Cards.object.all()
+    return render(request, 'cards/cards_list.html', {'cards': cards})
