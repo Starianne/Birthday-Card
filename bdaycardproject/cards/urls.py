7fc662159bdcb93188803f3547231051ba1b09ea
Annelies <118388665+Starianne@users.noreply.github.com>
2025-11-05 22:34:21 +0000

    cards as separate pages

--- bdaycardproject/cards/urls.py
+++ bdaycardproject/cards/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'cards'
 urlpatterns = [
-    path('', views.cards_list, name="cards"),
+    path('', views.cards_list, name="list"),
     path('<slug:slug>', views.card_page, name="page"),
 ]
