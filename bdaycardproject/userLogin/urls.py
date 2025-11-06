b47dca43d009941417ac0a9404cbd302bdeba9cc
Annelies <118388665+Starianne@users.noreply.github.com>
2025-11-06 20:47:28 +0000

    Fixing register page, put a slash in the wrong place :(

--- bdaycardproject/userLogin/urls.py
+++ bdaycardproject/userLogin/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'userLogin'
+
 urlpatterns = [
-    path('/register', views.register)
+    path('register/', views.register_view, name="register")
 ]
