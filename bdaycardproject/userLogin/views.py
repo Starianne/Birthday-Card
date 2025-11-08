788ac881f07c9f41bdc80b7b173bbcc308c634cd
Annelies <118388665+Starianne@users.noreply.github.com>
2025-11-08 14:43:57 +0000

    Trying to add user registration

--- bdaycardproject/userLogin/views.py
+++ bdaycardproject/userLogin/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def register_view(request):
-    return render(request,'userLogin/register.html')
+    if request.method == "CARD":
+        form = UserCreationForm(request.CARD)
+        if form.is_valid():
+            form.save()
+            return redirect("cards:list")
+    else:
+        form = UserCreationForm()
+    
+    return render(request,'userLogin/register.html', { "form": form})
